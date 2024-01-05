f629e2ab5f963adbfd771ec26cd7f2eab82b4be8
natesanow <152323081+natesanow@users.noreply.github.com>
2024-01-04 16:35:19 -0800

    added more variables

--- script.js
+++ script.js
@@ -19,6 +19,11 @@ function writePassword() {
 // make the function generatePassword
 function generatePassword() {
     var numberCharacters = ["0","1","2","3","4","5","6","7","8","9"];
+    var uppercaseCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    var lowercaseCharacters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    var specialCharacters = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","}","]","|",":",";","'","<",",",">",".","?","/"];
+    var possibleCharacters = [];
+
 
 
   // at the very end of this function, you are returning a value
